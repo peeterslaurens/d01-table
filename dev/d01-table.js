63003be1ffc428835e9a735d508fe06dae247c9e
Jeroen Valcke <valcke_jeroen@hotmail.com>
2016-07-19 14:52:58 +0200

    fix itemsPerPage error when nog pagination is present in the config

--- dev/d01-table.js
+++ dev/d01-table.js
@@ -61,7 +61,7 @@
                         }
 
                         var initializePagination = function initializePagination () {
-                            $scope.tablestatus.itemsPerPage = $scope.tableconfig.pagination.itemsPerPage;
+                            $scope.tablestatus.itemsPerPage = $scope.tableconfig.pagination && $scope.tableconfig.pagination.itemsPerPage ? $scope.tableconfig.pagination.itemsPerPage : $scope.tablesource.length;
                             $scope.tablestatus.pages = Math.ceil($scope.tablesource.length / $scope.tableconfig.pagination.itemsPerPage);
                         };
 
